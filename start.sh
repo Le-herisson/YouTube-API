9cd0939bbac20145d28eea1858e61a3b65cd5b3f
Le-herisson <168847121+Le-herisson@users.noreply.github.com>
2025-11-29 23:01:13 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-venv\Scripts\activate & uvicorn --host 127.0.0.1 --port 2684 --reload --log-level info --use-colors main:app & venv\Scripts\deactivate
+ uvicorn --host 127.0.0.1 --port 2684 --reload --log-level info --use-colors main:app
