08478d01a294f49c7bdaac33c576a862896b7df2
Le-herisson <168847121+Le-herisson@users.noreply.github.com>
2025-12-22 17:19:30 +0100

    Add debug scripts for local API debugging
    Introduced debug.cmd for Windows and debug.sh for Unix-based systems to simplify debugging the API locally on port 2684.

---
+++ debug.cmd
@@ -0,0 +1,5 @@
+@ECHO off
+TITLE API on 127.0.0.1:2684
+ECHO Open http://127.0.0.1:2684/
+venv\Scripts\activate.bat & uvicorn --host 0.0.0.0 --port 2684 --log-level info --use-colors --reload main:app & venv\Scripts\deactivate
+EXIT/B
---
+++ debug.sh
@@ -0,0 +1,1 @@
+ uvicorn --host 0.0.0.0 --port 2684 --log-level info --use-colors --reload main:app
